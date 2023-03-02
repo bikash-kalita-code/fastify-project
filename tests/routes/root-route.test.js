fb46abbaf4940af27b09c2247f48c7ec503c92df
Bikash Kalita <bikashkalita775@gmail.com>
2023-03-02 16:22:18 +0530

    Made few modifications and configured jest for testing

---
+++ tests/routes/root-route.test.js
@@ -0,0 +1,24 @@
+const { removeAllListeners } = require('nodemon');
+const build = require('../../src/app');
+
+let app;
+describe('Root route', () => {
+  // Before running any test cases
+  beforeEach(() => {
+    app = build();
+  });
+
+  // After running each test
+  afterEach(() => {
+    app.close();
+  });
+
+  it('should return 200 when root route called', async () => {
+    const res = await app.inject({
+      url: '/',
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({ hello: 'world!' });
+  });
+});
